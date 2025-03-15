3c40ac16970cade71ef900799392c6d42fb5d834
FahimHasan683031 <fahimhasan683031@gmail.com>
2025-03-16 03:59:03 +0600

    Branch api updated

--- src/app/modules/Branch/branch.interface.ts
+++ src/app/modules/Branch/branch.interface.ts
@@ -1,6 +1,27 @@
+import { Model } from "mongoose";
 export type TBranch = {
   name: string;
+  branchManagerName: string;
+  password: string;
   phone: string;
+  email: string;
   address: string;
-  status: "Active" | "Inactive";
+  status: "Active" | "Disabled";
+  role:"Branch"
+  createdAt: Date;
 };
+
+
+
+
+export interface BranchModel extends Model<TBranch> {
+
+  //instance methods for checking if passwords are matched
+  isPasswordMatched(
+    plainTextPassword: string,
+    hashedPassword: string,
+  ): Promise<boolean>;
+   //instance methods for checking if the Branch man exist
+  isBranchExists(paylod:string): Promise<TBranch>;
+ 
+}
--- src/app/modules/Branch/branch.validation.ts
+++ src/app/modules/Branch/branch.validation.ts
@@ -1,17 +1,25 @@
 import { z } from "zod";
 
 export const CrateBranchValidation = z.object({
-  name: z.string().min(1, "Name is required"),
-  phone: z.string().min(10, "Phone must be at least 10 digits"),
+  name: z.string().min(1, "Branch name is required"),
+  branchManagerName: z.string().min(1, "Branch manager name is required"),
+  password: z.string().min(6, "Password must be at least 6 characters long"),
+  phone: z.string(),
+  email: z.string().email("Invalid email address"),
   address: z.string().min(1, "Address is required"),
-  status: z.enum(["Active", "Inactive"]).default("Active"),
+  status: z.enum(["Active", "Disabled"]).default("Active"),
+  role:z.string().optional()
 });
 
 export const UpadateBranchValidation = z.object({
-  name: z.string().min(1, "Name is required").optional(),
-  phone: z.string().min(10, "Phone must be at least 10 digits").optional(),
-  address: z.string().min(1, "Address is required").optional(),
-  status: z.enum(["Active", "Inactive"]).optional(),
+  name: z.string().optional(),
+  branchManagerName: z.string().optional(),
+  password: z.string().optional(),
+  phone: z.string().optional(),
+  email: z.string().email("Invalid email address").optional(),
+  address: z.string().optional(),
+  status: z.enum(["Active", "Disabled"]).optional(),
+  role:z.string().optional()
 });
 export const BarnchValidations = {
   CrateBranchValidation,
